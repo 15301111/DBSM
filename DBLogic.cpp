c2c8f6e21dc4ca6aee2984e7b9131921156ce747
xnni <654879744@qq.com>
2017-05-23 21:13:15 +0800

    Add files via upload

--- DBLogic.cpp
+++ DBLogic.cpp
@@ -1,72 +1,127 @@
-#include "StdAfx.h"
+#include "stdafx.h"
 #include "DBLogic.h"
+#include "DBDAO.h"
+#include "Global.h"
+#include "TableEntity.h"
+#include "TableDAO.h"
+#include "SystemLogic.h"
 
-/**************************************************
-[FunctionName]	CreateDatabase
-[Function]	Create database
-[Argument]	CDBEntity &db: Database entity object, contains the name of the database.
-[ReturnedValue]	bool: True if the operation is successful, other false.
-**************************************************/
-bool CDBLogic::CreateDatabase(CDBEntity &db)
+CDBLogic::CDBLogic(void)
 {
-	try 
-	{
-		// Decide whether the file exists, if there is no,a file will be created.
-		CString strDBFile = m_fileLogic.GetDBFile();
-		if (CFileHelper::IsValidFile(strDBFile) == false)
-		{
-			if(CFileHelper::CreateFile(strDBFile) == false)
-			{
-				return false;
-			}
-		}
-
-		// Read file, decide whether exists the database of the same name
-		if (m_daoDB.GetDatabase(strDBFile, db) == true)
-		{
-			return false;
-		}
-
-		// Create database
-		CString strDBFolder = m_fileLogic.GetDBFolder(db.GetName());
-		db.SetFilepath(strDBFolder);
-		if(m_daoDB.Create(strDBFile, db) == false)
-		{
-			return false;
-		}
-	}
-	catch (CAppException* e)
-	{
-		throw e;
-	}
-	catch (...)
-	{
-		throw new CAppException(_T("Failed to create database!"));
-	}
-
-	return true;
+	this->sysPath=DEFAULT_SYS_FILE;
 }
 
-/**************************************************
-[FunctionName]	GetDatabase
-[Function]	Query the database information
-[Argument]	CDBEntity &db: Database entity object, contains the name of the database.
-		Queried database information will be saved to the object.
-[ReturnedValue]	bool: True if exists the database of the same name, otherwise false
-**************************************************/
-bool CDBLogic::GetDatabase(CDBEntity &db)
+
+CDBLogic::~CDBLogic(void)
 {
-	try
+}
+
+
+vector<CDBEntity> CDBLogic::GetDatabaseList()
+{
+	vector<CDBEntity> list=CDBDAO::ReadDBList(sysPath);
+	return list;
+}
+
+//接受一个数据库名，创建数据库
+int CDBLogic::CreateDatabase(CString &dbName)
+{
+	vector<CDBEntity> dblist=CDBDAO::ReadDBList(sysPath);
+	bool bExist=false;
+	for (vector<CDBEntity>::iterator ite=dblist.begin();ite!=dblist.end();++ite)
 	{
-		return m_daoDB.GetDatabase(m_fileLogic.GetDBFile(), db);
+		if(ite->GetName()==dbName)
+		{
+			bExist=true;
+			break;
+		}
 	}
-	catch (CAppException* e)
+
+	if (!bExist)
 	{
-		throw e;
+		int counter=CDBDAO::ReadDBCounter(sysPath);
+		CDBEntity db(++counter,dbName,DB_USER);
+		//MessageBox(NULL,CUtil::IntegerToString(counter),L"",MB_OK);
+		//添加记录到系统文件
+		if(!CDBDAO::WriteAnDB(db,sysPath))
+			return ADD_ERROR;
+		
+		//初始化db文件
+		if(!CDBDAO::InitDBFile(dbName))
+			return INITIATE_ERROR;
+		
+		if(!CDBDAO::SaveDBCounter(counter))
+			return SAVE_COUNTER_ERROR;
+
+		CSystemLogic sysLogic;
+		sysLogic.WriteLog(CString("created database:")+dbName);
+
+		return YES;
 	}
-	catch (...)
+	else
 	{
-		throw new CAppException(_T("Failed to create database!"));
+		return DB_EXIST;
 	}
-	return false;
+}
+
+//接受一个数据库新名和数据库id，修改数据库
+int CDBLogic::ModifyDBName(CString &oldDBName,CString &newDBName)
+{
+	vector<CDBEntity> dblist=CDBDAO::ReadDBList(sysPath);
+	bool bExist=false;
+	for (vector<CDBEntity>::iterator ite=dblist.begin();ite!=dblist.end();++ite)
+	{
+		if(ite->GetName()==newDBName)
+		{
+			bExist=true;
+			break;
+		} 
+	}
+
+	if (bExist)
+		return DB_EXIST;
+	else
+	{
+		if(!CDBDAO::ModifyDBName(oldDBName,newDBName,DEFAULT_SYS_FILE)||
+		   !CDBDAO::ModifyDirName(oldDBName,newDBName))
+			return MODIFY_ERROR;
+		else
+		{
+
+			CSystemLogic sysLogic;
+			sysLogic.WriteLog(CString("modified database name,")+oldDBName+CString("->")+newDBName);
+			return YES;
+		}
+			
+	
+	}
+}
+
+//删除指定的数据库
+int CDBLogic::DeleteDatabase(CString &dbname)
+{
+	if(!CDBDAO::DeleteDBRecord(this->sysPath,dbname)||!CDBDAO::DeleteDBFile(dbname))
+		return DELETE_ERROR;
+	else
+	{
+		CSystemLogic sysLogic;
+		sysLogic.WriteLog(CString("deleted database:")+dbname);
+		return YES;
+	}
+		
+}
+
+
+bool CDBLogic::BackupDatabase(CString &dbName,CString &destPath)
+{
+	CSystemLogic sysLogic;
+	sysLogic.WriteLog(CString("backuped database:")+dbName);
+	return CDBDAO::BackupDB(dbName,destPath);
+}
+
+bool CDBLogic::ImportDatabase(CString &path,CString &dbName)
+{
+	CSystemLogic sysLogic;
+	sysLogic.WriteLog(CString("imported database:")+dbName);
+	return CDBDAO::ImportDB(path,dbName);
 }
--- DBLogic.h
+++ DBLogic.h
@@ -1,21 +1,22 @@
 #pragma once
+#include "Global.h"
 #include "DBEntity.h"
-#include "DBDao.h"
-#include "FileLogic.h"
 
-/**************************************************
-[ClassName]	CDBLogic
-[Function]	Database definition business logic class
-**************************************************/
 class CDBLogic
 {
-public:
-	// Create database
-	bool CreateDatabase(CDBEntity &db);
-	// Query database
-	bool GetDatabase(CDBEntity &db);
-
 private:
-	CDBDao m_daoDB;			// The database data operation object
-	CFileLogic m_fileLogic;	// The logical object of the database file
+	CString sysPath;
+public:
+	CDBLogic(void);
+	~CDBLogic(void);
+
+	int CreateDatabase(CString &dbName); //接受一个数据库名，创建数据库
+	int ModifyDBName(CString &oldDBName,CString &newDBName);//接受一个数据库新名和数据库id，修改数据库
+	int DeleteDatabase(CString &dbname);//删除指定的数据库
+	vector<CDBEntity> GetDatabaseList();	//得到数据库列表
+
+	bool BackupDatabase(CString &dbName,CString &destPath);
+	bool ImportDatabase(CString &path,CString &dbName);
+
 };
+
